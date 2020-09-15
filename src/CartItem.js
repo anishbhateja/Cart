ecfc35b46feb29bb51380a8b97cf16cefe85948f
Anish Bhateja <anishbhateja99@gmail.com>
2020-09-15 12:35:06 +0530

    Added props to cart item

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,6 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: '',
-    };
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    //setState is synchronous in promises,Ajax, no batching is performed, component will be rendered every time setState is called
-  }
-
   // testing () {
   //   const promise = new Promise((resolve, reject) => {
   //     setTimeout(() => {
@@ -61,8 +49,8 @@ class CartItem extends React.Component {
     });
   };
   render() {
-    console.log('render');
-    const { price, title, qty } = this.state;
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
         <div className="left-block">
